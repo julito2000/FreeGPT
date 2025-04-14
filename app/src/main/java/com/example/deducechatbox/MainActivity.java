2bc3c2f1fdd5bf8d8bfe117f365b63fca1633487
julito2000 <ju_lito2000@hotmail.com>
2025-04-14 15:29:11 +0200

    Add Sppiner to choose the API

--- app/src/main/java/com/example/deducechatbox/MainActivity.java
+++ app/src/main/java/com/example/deducechatbox/MainActivity.java
@@ -4,22 +4,18 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Spinner;
 
 import androidx.room.Room;
 
-import com.example.deducechatbox.AppDatabase;
-import com.example.deducechatbox.MessageEntity;
-import com.example.deducechatbox.MessageDao;
-import com.example.deducechatbox.ChatAdapter;
-import com.google.gson.annotations.SerializedName;
-
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 import okhttp3.OkHttpClient;
 import okhttp3.logging.HttpLoggingInterceptor;
@@ -29,7 +25,6 @@ import retrofit2.Response;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 import retrofit2.http.Body;
-import retrofit2.http.Header;
 import retrofit2.http.POST;
 
 public class MainActivity extends AppCompatActivity {
@@ -43,6 +38,8 @@ public class MainActivity extends AppCompatActivity {
     private final String API_KEY = "Bearer sk-4bb15b7bb4044719aa3c1a1fc34263eb";
     private AppDatabase db;
     private MessageDao messageDao;
+    private Spinner selApi;
+    private String model ="";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -52,13 +49,26 @@ public class MainActivity extends AppCompatActivity {
         userInput = findViewById(R.id.userInput);
         sendButton = findViewById(R.id.sendButton);
         recyclerView = findViewById(R.id.recyclerView);
+        selApi = findViewById(R.id.selecAPI);
 
         chatAdapter = new ChatAdapter(chatMessages);
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
         recyclerView.setAdapter(chatAdapter);
 
+
         db = Room.databaseBuilder(getApplicationContext(), AppDatabase.class, "chat-db").allowMainThreadQueries().build();
         messageDao = db.messageDao();
+
+        // Create an ArrayAdapter using the string array and a default spinner layout.
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(
+                this,
+                R.array.api_array,
+                android.R.layout.simple_spinner_item
+        );
+        // Specify the layout to use when the list of choices appears.
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        // Apply the adapter to the spinner.
+        selApi.setAdapter(adapter);
 
         // Load previous messages
         chatMessages.addAll(messageDao.getAllMessages());
@@ -77,6 +87,8 @@ public class MainActivity extends AppCompatActivity {
                                     .build()
                     );
                 })
+                .connectTimeout(0, TimeUnit.SECONDS)
+                .readTimeout(0,TimeUnit.SECONDS)
                 .build();
 
         Retrofit retrofit = new Retrofit.Builder()
@@ -87,29 +99,48 @@ public class MainActivity extends AppCompatActivity {
 
         openAIApi = retrofit.create(OpenAIApi.class);
 
+        selApi.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+
+                model = (String) selApi.getItemAtPosition(i);
+
+
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+
+            }
+        });
         sendButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 String input = userInput.getText().toString();
                 if (!input.isEmpty()) {
                     addMessageToChat("user", input);
-                    sendMessage(input,0.0,0.0,0,0,0.0);
+                    sendMessage(input,model,0.0,0.0,0,0,0.0);
                     userInput.setText("");
                 }
             }
         });
+
     }
 
-    private void sendMessage(String inputText, Double temperature, Double topK, int topP, int max_token,double rep_penalty ) {
+
+
+    private void sendMessage(String inputText,String model, Double temperature, Double topK, int topP, int max_token,double rep_penalty ) {
         Message userMessage = new Message("user", inputText);
 
+        //Pasamos el historico de la conversacion a la API para que recuerde lo que hemos hablado
         List<Message> fullConversation = new ArrayList<>();
         for (MessageEntity msg : chatMessages) {
             fullConversation.add(new Message(msg.getRole(), msg.getContent()));
         }
         fullConversation.add(userMessage); // también añadimos el nuevo mensaje
 
-        OpenAIRequest request = new OpenAIRequest("llama3.1:latest", fullConversation);
+
+        OpenAIRequest request = new OpenAIRequest(model, fullConversation);
         //OpenAIRequest request = new OpenAIRequest("llama3.1:latest", Arrays.asList(userMessage));
 
         openAIApi.sendMessage(request).enqueue(new Callback<OpenAIResponse>() {
